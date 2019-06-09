e0f92b468574f0e36bf4a9e9683af023c18dbf03
Anil Maurya <anil@joshsoftware.com>
2019-06-09 16:47:49 +0530

    Updates

---
+++ precache-manifest.56bbb71cf05d65c90a856518e4d85ae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f1cce6f44df4241b0c",
+    "url": "/link-shortner/static/css/main.1270eaa4.chunk.css"
+  },
+  {
+    "revision": "b5f1cce6f44df4241b0c",
+    "url": "/link-shortner/static/js/main.b5f1cce6.chunk.js"
+  },
+  {
+    "revision": "5c9423de18ce02e886f6",
+    "url": "/link-shortner/static/js/1.5c9423de.chunk.js"
+  },
+  {
+    "revision": "581b2f240e92e114d2b5",
+    "url": "/link-shortner/static/js/runtime~main.581b2f24.js"
+  },
+  {
+    "revision": "3427d8da2312080476cd69c0271501cc",
+    "url": "/link-shortner/index.html"
+  }
+];
