a76ff69311664483e80851b781b745d76ebb2fa7
Anil Maurya <anil@joshsoftware.com>
2019-06-10 18:58:47 +0530

    Configure and deploy smart contract on Ropsten test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,29 @@
 const path = require("path");
+require('dotenv').config()
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const MNEMONIC = process.env.MNEMONIC
+const ROPSTEN_URL = process.env.ROPSTEN_URL
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, ROPSTEN_URL);
+      },
+      network_id: '3',
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+   },
+   test: {
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*",
+  }
+ }
 };
